c33a4d9443b085334ccf5b3ff6a0753a0642f961
Will Strinz <wstrinz@gmail.com>
2016-12-10 21:05:48 -0600

    play around with some process stuff

--- lib/mystery_potion.ex
+++ lib/mystery_potion.ex
@@ -8,17 +8,40 @@ defmodule MysteryPotion do
     |> Map.get("session_id")
   end
 
-  def get_feeds(session_id, category) do
-    HTTPoison.post!(@api_url, ~s({"sid":"#{session_id}","op":"getFeeds","cat_id":"#{category}","unread_only":"true"})).body
+  def start_agent do
+    FeedAgent.run(login("user", "pass"))
+  end
+
+  def run do
+    pid = spawn(&MysteryPotion.start_agent/0)
+    send(pid, {:get_random_feed, self()})
+    receive do r -> r end
+  end
+end
+
+defmodule FeedAgent do
+  @api_url "http://feeds.stri.nz/api/"
+
+  def get_feeds(token, category) do
+    HTTPoison.post!(@api_url, ~s({"sid":"#{token}","op":"getFeeds","cat_id":"#{category}","unread_only":"true"})).body
     |> Poison.decode!
     |> Map.get("content")
   end
 
-  def random_feed(user, pass) do
-    get_feeds(login(user, pass), "-4") |> Enum.random
+  def random_article(token) do
+    random_feed(token)
+    |> Map.get("id")
+    # ...
   end
 
-  def run do
-    random_feed("user", "pass")
+  def random_feed(token) do
+    get_feeds(token, "-4") |> Enum.random
+  end
+
+  def run(session_id) do
+    receive do
+      {:get_random_feed, from} ->
+        send(from, random_feed(session_id))
+    end
   end
 end
